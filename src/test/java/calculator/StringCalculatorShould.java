94ae9bc56e7f7cc7b57846b5326241547886c6f1
MohitKumar062 <88274238+MohitKumar062@users.noreply.github.com>
2021-10-18 08:52:55 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,73 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
-
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+		StringCalculator strCal;
+		
+		@BeforeEach
+		public void init() {
+			strCal= new StringCalculator();			
+			}
+		//1
+		@Test
+		void StringIsEmptyReturnZero() throws NegativeNumberException {
+			assertEquals(0,strCal.Add(""));
+			
+		}
+		//2
+		@Test
+		void StringHavingSingleDigitReturnDigit() throws NegativeNumberException {
+			assertEquals(3,strCal.Add("3"));
+			}
+		//3
+		@Test
+		void StringHavingTwoDigitWithCommaDelimeterReturnSum() throws NegativeNumberException {
+			assertEquals(5,strCal.Add("2,3"));
+			}
+		//4
+		@Test
+		void StringHavingLargeNumberWithCommaDelimeterReturnSum() throws NegativeNumberException {
+			assertEquals(100,strCal.Add("3,3,4,2,6,7,12,10,13,40"));
+			}
+		//5
+		@Test
+		void StringHavingTwoDigitWithSemiColnDelimeterReturnSum() throws NegativeNumberException {
+			assertEquals(4,strCal.Add("1;3"));
+		
+		
+			}
+		//6
+		@Test
+		void StringHavingThreeDigitWithCommaDelimeterAndLineChangeReturnSum() throws NegativeNumberException {
+			assertEquals(8,strCal.Add("3\n,3,2"));
+			}
+		//7
+		@Test
+		void StringHavingNumberThousandOrAboveGreaterThenReturnSum() throws NegativeNumberException {
+			assertEquals(8,strCal.Add("1000,1,3,4"));
+			}
+		//8
+		@Test
+		public void StringWithNegativeNumThrowsException() {
+		    Throwable exception = assertThrows(NegativeNumberException.class,
+		          ()->{strCal.Add("[#]\n-2#1");} );
+		}
+		//9
+		@Test
+		public void DelimeterOfAnyLength()throws NegativeNumberException {
+			assertEquals(6,strCal.Add("[***]\n1***2***3"));
+			}
+		//10
+		@Test
+		public void DelimeterOfAnyLengthAndNumberAreBig()throws NegativeNumberException {
+			assertEquals(10,strCal.Add("[****]\n1****2****3****1002****4"));
+			}
+		
 }
+ 
